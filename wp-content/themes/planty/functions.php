f579d62f8ba81df4335572e065e7ba32f0323576
16 Caracteres <camille@16caracteres.com>
2024-01-08 16:56:18 +0100

    Ajout navigation et hook dans functions.php

--- wp-content/themes/planty/functions.php
+++ wp-content/themes/planty/functions.php
@@ -3,3 +3,21 @@
     wp_enqueue_style('style', get_template_directory_uri() . '/style.css');
    }
    add_action( 'wp_enqueue_scripts', 'planty_theme' );
+
+// Register nav menus.
+   register_nav_menus(
+      array(
+         'primary' => __( 'Primary Navigation', 'planty' ),
+      )
+   );
+
+// Hook Admin
+function display_admin($items, $args) {
+    if ($args->theme_location == 'primary') {
+        if (is_user_logged_in()) {
+            $items .= '<li><a href="#">Admin</a></li>';
+        }
+    }
+    return $items;
+}
+apply_filters('wp_nav_menu_items', 'display_admin', 10, 2);
