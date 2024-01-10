972009beda4a16e85823dbbe955fca7694d89675
16 Caracteres <camille@16caracteres.com>
2024-01-10 10:43:03 +0100

    Hook fonctionnel pour l'affichage d'admin

--- wp-content/themes/planty/functions.php
+++ wp-content/themes/planty/functions.php
@@ -5,19 +5,28 @@
    add_action( 'wp_enqueue_scripts', 'planty_theme' );
 
 // Register nav menus.
-   register_nav_menus(
+/*   register_nav_menus(
       array(
          'primary' => __( 'Primary Navigation', 'planty' ),
       )
    );
-
+*/
 // Hook Admin
-function display_admin($items, $args) {
+/*function display_admin($items, $args) {
     if ($args->theme_location == 'primary') {
         if (is_user_logged_in()) {
-            $items .= '<li><a href="#">Admin</a></li>';
+            $items .= '<li><a href="<?php echo admin_url(); ?>">Admin</a></li>';
         }
     }
     return $items;
 }
-apply_filters('wp_nav_menu_items', 'display_admin', 10, 2);
+add_filter('wp_nav_menu_items', 'display_admin', 10, 2);*/
+
+function remove_admin_item(){
+	if (!is_user_logged_in()) {?>
+	<script>
+		 document.querySelectorAll('.wp-block-navigation-item')[2].remove()
+	</script>
+<?php }
+}
+add_action('wp_footer', 'remove_admin_item');
